5e8ffd33648de08bce8989bdd8ca605af20ac86e
Scytheria2017 <robert.hicks@wellingtoncollege.cn>
2025-10-24 13:30:53 +0800

    A003

--- Main.py
+++ Main.py
@@ -1,13 +1,37 @@
-import TEC-Database
-import TEC-Jabberwocky
-import TEC-Logging
-import TEC-Opcodes
+import Components.DefineJabberwocky
+import Components.DefineOpcodes
 
 import asyncio
 import logging
 import os
 import struct
 import subprocess
+
+
+
+# -----------------
+# Configure logging
+# -----------------
+
+logging.basicConfig(
+    level=logging.INFO,
+    format=' %(message)s',
+    handlers=[
+        logging.FileHandler('wow_server.log'),
+        logging.StreamHandler()
+    ]
+)
+
+
+# -------------
+# Debug Logging
+# -------------
+
+debug = True
+
+def debug_logging(text):
+    if debug:
+        logging.info(f" DEBUG: {text}")
 
 
 # ----------------
